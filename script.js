fd7a6f31a69f4269660fc4c22a5a95747c4c5845
Donald <doniecode@gmail.com>
2025-10-14 03:02:19 +0200

    some edits

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const menubtns = document.querySelector(".menu-btns");
 const hamburger = document.querySelector(".hamburger");
 const closemenu = document.querySelector(".close-menu");
 const sidebar = document.querySelector(".sidebar");
+const header = document.querySelector(".main-header");
 const copyrightmsg = document.querySelector(".copyright-msg .date");
 
 copyrightmsg.innerHTML = new Date().getFullYear();
 
 hamburger.addEventListener("click", function(){
-  sidebar.style.display="flex";
+  sidebar.classList.add("active");
   hamburger.style.display="none";
   closemenu.style.display="block";
 })
 
 closemenu.addEventListener("click", function(){
-  sidebar.style.display="none";
+  sidebar.classList.remove("active");
   hamburger.style.display="block";
   closemenu.style.display="none";
 })
@@ -27,5 +28,13 @@ window.addEventListener('scroll', function(){
     toTop.classList.add('active');
   } else{
     toTop.classList.remove('active');
+  }
+})
+
+window.addEventListener('scroll', function(){
+  if(window.pageYOffset > 300){
+    header.classList.add("bg");
+  } else{
+    header.classList.remove("bg");
   }
 })
